a2e5d257d2c683c971f80b30d2cb805d28bef172
Philipp Conrad <philipp8101@gmail.com>
2022-11-09 21:19:39 +0100

    fixed paths in fzf.zsh

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,13 +1,15 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/philipp/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/philipp/.fzf/bin"
+if [[ ! "$PATH" == *~/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}~/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/philipp/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "~/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/philipp/.fzf/shell/key-bindings.zsh"
+if [[ -e  "~/.fzf/shell/key-bindings.zsh" ]]; then 
+	source "~/.fzf/shell/key-bindings.zsh"
+fi
