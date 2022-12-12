1b38bccd31e41574ba3b3560975f51ed2dd28c31
Philipp Conrad <philipp8101@gmail.com>
2022-12-12 18:52:07 +0100

    added nvim-dap + reformat plugin.lua

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -3,16 +3,16 @@ local fn = vim.fn
 -- Automatically install packer
 local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-	PACKER_BOOTSTRAP = fn.system {
-		"git",
-		"clone",
-		"--depth",
-		"1",
-		"https://github.com/wbthomason/packer.nvim",
-		install_path,
-	}
-	print "Installing packer close and reopen Neovim..."
-	vim.cmd [[packadd packer.nvim]]
+    PACKER_BOOTSTRAP = fn.system {
+        "git",
+        "clone",
+        "--depth",
+        "1",
+        "https://github.com/wbthomason/packer.nvim",
+        install_path,
+    }
+    print "Installing packer close and reopen Neovim..."
+    vim.cmd [[packadd packer.nvim]]
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
@@ -26,347 +26,199 @@ vim.cmd [[
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-	return
+    return
 end
 
 -- Have packer use a popup window
 packer.init {
-	display = {
-		open_fn = function()
-			return require("packer.util").float { border = "rounded" }
-		end,
-	},
+    display = {
+        open_fn = function()
+            return require("packer.util").float { border = "rounded" }
+        end,
+    },
 }
 
 -- Install your plugins here
 return packer.startup(function(use)
-	-- My plugins here
-	use "wbthomason/packer.nvim" -- Have packer manage itself
-	use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
-	use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
-
-	use({
-		'Wansmer/treesj',
-		requires = { 'nvim-treesitter' },
-		config = function()
-			require('treesj').setup({
-				-- Use default keymaps
-				-- (<space>m - toggle, <space>j - join, <space>s - split)
-				use_default_keymaps = false,
-
-				-- Node with syntax error will not be formatted
-				check_syntax_error = true,
-
-				-- If line after join will be longer than max value,
-				-- node will not be formatted
-				max_join_length = 120,
-
-				-- hold|start|end:
-				-- hold - cursor follows the node/place on which it was called
-				-- start - cursor jumps to the first symbol of the node being formatted
-				-- end - cursor jumps to the last symbol of the node being formatted
-				cursor_behavior = 'hold',
-
-				-- Notify about possible problems or not
-				notify = true,
-				langs = {--[[ configuration for languages ]]},
-			})
-		end,
-	})
-
-	use {
-		'numToStr/Comment.nvim',
-		config = function()
-			require('Comment').setup({
-				---Add a space b/w comment and the line
-				padding = true,
-				---Whether the cursor should stay at its position
-				sticky = true,
-				---Lines to be ignored while (un)comment
-				ignore = nil,
-				---LHS of toggle mappings in NORMAL mode
-				toggler = {
-					---Line-comment toggle keymap
-					line = 'gcc',
-					---Block-comment toggle keymap
-					block = 'gbc',
-				},
-				---LHS of operator-pending mappings in NORMAL and VISUAL mode
-				opleader = {
-					---Line-comment keymap
-					line = 'gc',
-					---Block-comment keymap
-					block = 'gb',
-				},
-				---LHS of extra mappings
-				extra = {
-					---Add comment on the line above
-					above = 'gcO',
-					---Add comment on the line below
-					below = 'gco',
-					---Add comment at the end of line
-					eol = 'gcA',
-				},
-				---Enable keybindings
-				---NOTE: If given `false` then the plugin won't create any mappings
-				mappings = {
-					---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
-					basic = true,
-					---Extra mapping; `gco`, `gcO`, `gcA`
-					extra = true,
-				},
-				---Function to call before (un)comment
-				pre_hook = nil,
-				---Function to call after (un)comment
-				post_hook = nil,
-			})
-		end
-	}
-
-	use { 'ethanholz/nvim-lastplace',
-		config = function()
-			require'nvim-lastplace'.setup({
-				lastplace_ignore_buftype = {"quickfix", "nofile", "help"},
-				lastplace_ignore_filetype = {"gitcommit", "gitrebase", "svn", "hgcommit"},
-				lastplace_open_folds = true
-			})
-		end
-	}
-
-	use {"akinsho/toggleterm.nvim",
-		tag = '*',
-		config = function()
-			require("toggleterm").setup()
-	end}
-
-	use {'stevearc/dressing.nvim',
-		config = function()
-			require('dressing').setup({
-				input = {
-					-- Set to false to disable the vim.ui.input implementation
-					enabled = true,
-
-					-- Default prompt string
-					default_prompt = "Input:",
-
-					-- Can be 'left', 'right', or 'center'
-					prompt_align = "left",
-
-					-- When true, <Esc> will close the modal
-					insert_only = true,
-
-					-- When true, input will start in insert mode.
-					start_in_insert = true,
-
-					-- These are passed to nvim_open_win
-					anchor = "SW",
-					border = "rounded",
-					-- 'editor' and 'win' will default to being centered
-					relative = "cursor",
-
-					-- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
-					prefer_width = 40,
-					width = nil,
-					-- min_width and max_width can be a list of mixed types.
-					-- min_width = {20, 0.2} means "the greater of 20 columns or 20% of total"
-					max_width = { 140, 0.9 },
-					min_width = { 20, 0.2 },
-
-					buf_options = {},
-					win_options = {
-						-- Window transparency (0-100)
-						winblend = 10,
-						-- Disable line wrapping
-						wrap = false,
-					},
-
-					-- Set to `false` to disable
-					mappings = {
-						n = {
-							["<Esc>"] = "Close",
-							["<CR>"] = "Confirm",
-						},
-						i = {
-							["<C-c>"] = "Close",
-							["<CR>"] = "Confirm",
-							["<Up>"] = "HistoryPrev",
-							["<Down>"] = "HistoryNext",
-						},
-					},
-
-					override = function(conf)
-						-- This is the config that will be passed to nvim_open_win.
-						-- Change values here to customize the layout
-						return conf
-					end,
-
-					-- see :help dressing_get_config
-					get_config = nil,
-				},
-				select = {
-					-- Set to false to disable the vim.ui.select implementation
-					enabled = true,
-
-					-- Priority list of preferred vim.select implementations
-					backend = { "telescope", "fzf_lua", "fzf", "builtin", "nui" },
-
-					-- Trim trailing `:` from prompt
-					trim_prompt = true,
-
-					-- Options for telescope selector
-					-- These are passed into the telescope picker directly. Can be used like:
-					-- telescope = require('telescope.themes').get_ivy({...})
-					telescope = nil,
-
-					-- Options for fzf selector
-					fzf = {
-						window = {
-							width = 0.5,
-							height = 0.4,
-						},
-					},
-
-					-- Options for fzf_lua selector
-					fzf_lua = {
-						winopts = {
-							width = 0.5,
-							height = 0.4,
-						},
-					},
-
-					-- Options for nui Menu
-					nui = {
-						position = "50%",
-						size = nil,
-						relative = "editor",
-						border = {
-							style = "rounded",
-						},
-						buf_options = {
-							swapfile = false,
-							filetype = "DressingSelect",
-						},
-						win_options = {
-							winblend = 10,
-						},
-						max_width = 80,
-						max_height = 40,
-						min_width = 40,
-						min_height = 10,
-					},
-
-					-- Options for built-in selector
-					builtin = {
-						-- These are passed to nvim_open_win
-						anchor = "NW",
-						border = "rounded",
-						-- 'editor' and 'win' will default to being centered
-						relative = "editor",
-
-						buf_options = {},
-						win_options = {
-							-- Window transparency (0-100)
-							winblend = 10,
-						},
-
-						-- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
-						-- the min_ and max_ options can be a list of mixed types.
-						-- max_width = {140, 0.8} means "the lesser of 140 columns or 80% of total"
-						width = nil,
-						max_width = { 140, 0.8 },
-						min_width = { 40, 0.2 },
-						height = nil,
-						max_height = 0.9,
-						min_height = { 10, 0.2 },
-
-						-- Set to `false` to disable
-						mappings = {
-							["<Esc>"] = "Close",
-							["<C-c>"] = "Close",
-							["<CR>"] = "Confirm",
-						},
-
-						override = function(conf)
-							-- This is the config that will be passed to nvim_open_win.
-							-- Change values here to customize the layout
-							return conf
-						end,
-					},
-
-					-- Used to override format_item. See :help dressing-format
-					format_item_override = {},
-
-					-- see :help dressing_get_config
-					get_config = nil,
-				},
-			})
-		end
-	}
-
-	use { 'mrjones2014/legendary.nvim' }
-
-	use {
-		"jiaoshijie/undotree",
-		config = function()
-			require('undotree').setup()
-		end,
-		requires = {
-			"nvim-lua/plenary.nvim",
-		},
-	}
-
-	use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' }
+    -- My plugins here
+    use "wbthomason/packer.nvim" -- Have packer manage itself
+    use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
+    use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
 
     use({
-        "kylechui/nvim-surround",
-        tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+        'Wansmer/treesj',
+        requires = { 'nvim-treesitter' },
         config = function()
-            require("nvim-surround").setup({
-                -- Configuration here, or leave empty to use defaults
+            require('treesj').setup({
+                use_default_keymaps = false,-- (<space>m - toggle, <space>j - join, <space>s - split)
+                check_syntax_error = true,
+                max_join_length = 120,
+                cursor_behavior = 'hold',
+                notify = true,
+                langs = {--[[ configuration for languages ]]},
             })
-        end
+        end,
     })
 
-	-- Colorschemes
-	use "folke/tokyonight.nvim"
-	use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
-	use 'vim-airline/vim-airline'
-	use 'vim-airline/vim-airline-themes'
+    use {
+        'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup({
+                padding = true,
+                sticky = true,
+                ignore = nil,
+                toggler = { line = 'gcc', block = 'gbc' },
+                opleader = { line = 'gc', block = 'gb' },
+                extra = { above = 'gcO', below = 'gco', eol = 'gcA' },
+                mappings = { basic = true, extra = true },
+                pre_hook = nil,
+                post_hook = nil,
+            })
+        end
+    }
 
-	-- cmp plugins
-	use "hrsh7th/nvim-cmp" -- The completion plugin
-	use "hrsh7th/cmp-buffer" -- buffer completions
-	use "hrsh7th/cmp-path" -- path completions
-	use "hrsh7th/cmp-cmdline" -- cmdline completions
-	use "saadparwaiz1/cmp_luasnip" -- snippet completions
-	use "hrsh7th/cmp-nvim-lsp"
+    use { 'ethanholz/nvim-lastplace',
+        config = function()
+            require'nvim-lastplace'.setup({
+                lastplace_ignore_buftype = {"quickfix", "nofile", "help"},
+                lastplace_ignore_filetype = {"gitcommit", "gitrebase", "svn", "hgcommit"},
+                lastplace_open_folds = true
+            })
+        end
+    }
 
-	-- snippets
-	use "L3MON4D3/LuaSnip" --snippet engine
-	use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+    use {"akinsho/toggleterm.nvim",
+        tag = '*',
+        config = function()
+            require("toggleterm").setup()
+    end}
 
-	-- LSP
-	use "neovim/nvim-lspconfig" -- enable LSP
-	use "williamboman/nvim-lsp-installer" -- simple to use language server installer
-	use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
+    use {'stevearc/dressing.nvim',
+        config = function()
+            require('dressing').setup({
+                input = {
+                    enabled = true,
+                    default_prompt = "Input:",
+                    prompt_align = "left",
+                    insert_only = true,
+                    start_in_insert = true,
+                    anchor = "SW",
+                    border = "rounded",
+                    relative = "cursor",
+                    prefer_width = 40,
+                    width = nil,
+                    max_width = { 140, 0.9 },
+                    min_width = { 20, 0.2 },
+                buf_options = {},
+                    win_options = {
+                        winblend = 10,
+                        wrap = false,
+                    },
+                    mappings = {
+                        n = { ["<Esc>"] = "Close", ["<CR>"] = "Confirm" },
+                        i = { ["<C-c>"] = "Close", ["<CR>"] = "Confirm", ["<Up>"] = "HistoryPrev", ["<Down>"] = "HistoryNext" },
+                    },
+                    override = function(conf) return conf end,
+                    get_config = nil,
+                },
+                select = {
+                    enabled = true,
+                    backend = { "telescope", "fzf_lua", "fzf", "builtin", "nui" },
+                    trim_prompt = true,
+                    telescope = nil,
+                    fzf = { window = { width = 0.5, height = 0.4 } },
+                    fzf_lua = { winopts = { width = 0.5, height = 0.4 } },
+                    nui = {
+                        position = "50%",
+                        size = nil,
+                        relative = "editor",
+                        border = { style = "rounded" },
+                        buf_options = { swapfile = false, filetype = "DressingSelect" },
+                        win_options = { winblend = 10 },
+                        max_width = 80,
+                        max_height = 40,
+                        min_width = 40,
+                        min_height = 10,
+                    },
+                    builtin = {
+                        anchor = "NW",
+                        border = "rounded",
+                        relative = "editor",
+                        buf_options = {},
+                        win_options = { winblend = 10 },
+                        width = nil,
+                        max_width = { 140, 0.8 },
+                        min_width = { 40, 0.2 },
+                        height = nil,
+                        max_height = 0.9,
+                        min_height = { 10, 0.2 },
+                        mappings = { ["<Esc>"] = "Close", ["<C-c>"] = "Close", ["<CR>"] = "Confirm" },
+                        override = function(conf) return conf end,
+                    },
+                    format_item_override = {},
+                    get_config = nil,
+                },
+            })
+        end
+    }
 
-	-- Telescope
-	use "nvim-telescope/telescope.nvim"
+    use { 'mrjones2014/legendary.nvim' }
 
-	--harpoon
-	use "ThePrimeagen/harpoon"
+    use { "jiaoshijie/undotree",
+        config = function() require('undotree').setup() end,
+        requires = { "nvim-lua/plenary.nvim" },
+    }
 
-	-- Treesitter
-	use {
-		"nvim-treesitter/nvim-treesitter",
-		run = ":TSUpdate",
-	}
-	use "p00f/nvim-ts-rainbow"
-	use "nvim-treesitter/playground"
+    use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' }
 
-	-- Automatically set up your configuration after cloning packer.nvim
-	-- Put this at the end after all plugins
-	if PACKER_BOOTSTRAP then
-		require("packer").sync()
-	end
+    use { "kylechui/nvim-surround",
+        tag = "*",
+        config = function() require("nvim-surround").setup({ }) end
+    }
+
+    use { 'mfussenegger/nvim-dap' }
+
+    -- Colorschemes
+    use "folke/tokyonight.nvim"
+    use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
+    use 'vim-airline/vim-airline'
+    use 'vim-airline/vim-airline-themes'
+    --[[ use({ 'xiyaowong/nvim-transparent'}) ]]
+
+    -- cmp plugins
+    use "hrsh7th/nvim-cmp" -- The completion plugin
+    use "hrsh7th/cmp-buffer" -- buffer completions
+    use "hrsh7th/cmp-path" -- path completions
+    use "hrsh7th/cmp-cmdline" -- cmdline completions
+    use "saadparwaiz1/cmp_luasnip" -- snippet completions
+    use "hrsh7th/cmp-nvim-lsp"
+
+    -- snippets
+    use "L3MON4D3/LuaSnip" --snippet engine
+    use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+
+    -- LSP
+    use "neovim/nvim-lspconfig" -- enable LSP
+    use "williamboman/nvim-lsp-installer" -- simple to use language server installer
+    use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
+
+    -- Telescope
+    use "nvim-telescope/telescope.nvim"
+
+    --harpoon
+    use "ThePrimeagen/harpoon"
+
+    -- Treesitter
+    use {
+        "nvim-treesitter/nvim-treesitter",
+        run = ":TSUpdate",
+    }
+    use "p00f/nvim-ts-rainbow"
+    use "nvim-treesitter/playground"
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if PACKER_BOOTSTRAP then
+        require("packer").sync()
+    end
 end)
