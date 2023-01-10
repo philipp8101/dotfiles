e80a7ab40c0cf75aab384d512cd9bc5d73e5bf45
Philipp Conrad <philipp8101@gmail.com>
2023-01-10 19:32:45 +0100

    fix formatoptions not applying

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,8 @@
 require "user.plugins"
+require "user.options"
 require "user.dap"
 require 'user.maps'
 require "user.cmp"
 require "user.lsp"
 require "user.telescope"
 require "user.treesitter"
-require "user.options"
