39e198c5d9e021f3a02e5bdeeb71ef580e503dac
Philipp Conrad <philipp8101@gmail.com>
2023-04-07 19:05:54 +0200

    updated polybar conf

--- .config/polybar/polybar-scripts/date.sh
+++ .config/polybar/polybar-scripts/date.sh
@@ -3,14 +3,14 @@
  
 case "$1" in
 --popup)
-	yad --calendar --undecorated --fixed --close-on-unfocus --no-buttons --posx=815 --posy=125        --title="calendar" --borders=0
+	yad --calendar --undecorated --fixed --close-on-unfocus --no-buttons --posx=445 --posy=95        --title="calendar" --borders=0
     ;;
 --tz)
 	notify-send "$(TZ=$(i3-input|grep -Po '(?<=output = ).+') date "+%Z - %a - KW%V - %d.%m.%Y %H:%M")"
 	;;
 *)
 	#date "+UTC%:::z(%Z) - %a - KW%V - %d.%m.%Y %H:%M"
-    date "+%{F#6e738d}KW%V -%{F-} %A - %d.%m.%Y - %H:%M %{F#6e738d}- %Z%{F-}"
+    date "+%{F#6e738d}KW%V "$(expr 52 + $(date +%V))"  -%{F-} %A - %d.%m.%Y - %H:%M:%S %{F#6e738d}- %Z%{F-}"
     ;;
 esac
  
