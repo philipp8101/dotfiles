e8d285ff08410216569b63f20fab79926e7ff32f
Philipp Conrad <philipp8101@gmail.com>
2023-04-25 11:11:45 +0200

    fix nvim-dap config

--- .config/nvim/lua/user/dap.lua
+++ .config/nvim/lua/user/dap.lua
@@ -9,32 +9,35 @@ dap.adapters.cppdbg = {
 
 dap.configurations.cpp = {
   {
-    name = "Launch file",
-    type = "cppdbg",
+    name = "Launch",
+    type = "lldb",
     request = "launch",
     program = function()
       return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
     end,
     cwd = '${workspaceFolder}',
-    stopAtEntry = true,
+    stopAtEntry = false,
+    args = {}
   },
-  {
-    name = 'Attach to gdbserver :1234',
-    type = 'cppdbg',
-    request = 'launch',
-    MIMode = 'gdb',
-    miDebuggerServerAddress = 'localhost:1234',
-    miDebuggerPath = '/usr/bin/gdb',
-    cwd = '${workspaceFolder}',
-    program = function()
-      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-    end,
-  },
+  -- {
+  --   name = 'Attach to gdbserver :1234',
+  --   type = 'cppdbg',
+  --   request = 'launch',
+  --   MIMode = 'gdb',
+  --   miDebuggerServerAddress = 'localhost:1234',
+  --   miDebuggerPath = '/usr/bin/gdb',
+  --   cwd = '${workspaceFolder}',
+  --   program = function()
+  --     return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+  --   end,
+  -- },
 }
+dap.configurations.c = dap.configurations.cpp
+dap.configurations.rust = dap.configurations.cpp
 
 dap.adapters.lldb = {
     type = 'executable',
-    command = '/usr/bin/lldb-mi',
+    command = '/usr/bin/lldb-vscode',
     name = 'lldb'
 }
 dap.adapters.python = {
