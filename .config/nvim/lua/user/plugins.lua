267dfc0234f14e89633efa5fadc181f66abab753
Philipp Conrad <philipp8101@gmail.com>
2023-04-26 21:57:50 +0200

    fix luasnip tab jump bug

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -264,7 +264,13 @@ return packer.startup(function(use)
     use "hrsh7th/cmp-nvim-lsp"
 
     -- snippets
-    use "L3MON4D3/LuaSnip" --snippet engine
+    use { "L3MON4D3/LuaSnip", config = function() 
+        require("luasnip").setup({
+            history = true,
+            region_check_events = "InsertEnter",
+            delete_check_events = "TextChanged,InsertLeave"
+        })
+    end } --snippet engine
     use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
     -- LSP
