f6c4136e7f0ae3d9045d8b8558c0b6f0469ace30
Philipp Conrad <philipp8101@gmail.com>
2023-05-10 00:57:11 +0200

    nvim: fixed treesitter to work properly with lazy

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -425,72 +425,132 @@ require("lazy").setup({
 
     { "ThePrimeagen/harpoon" },
 
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = function() vim.cmd("TSUpdate") end,
-        opts = {
-            ensure_installed = "all",
-            sync_install = false, 
-            ignore_install = { "" }, -- List of parsers to ignore installing
-            highlight = {
-                enable = true, -- false will disable the whole extension
-                disable = { "" }, -- list of language that will be disabled
-                additional_vim_regex_highlighting = true,
+    { "nvim-treesitter/nvim-treesitter",
+        version = false, -- last release is way too old and doesn't work on Windows
+        build = ":TSUpdate",
+        event = { "BufReadPost", "BufNewFile" },
+        dependencies = {
+            {
+                "nvim-treesitter/nvim-treesitter-textobjects",
+                init = function()
+                    -- PERF: no need to load the plugin, if we only need its queries for mini.ai
+                    local plugin = require("lazy.core.config").spec.plugins["nvim-treesitter"]
+                    local opts = require("lazy.core.plugin").values(plugin, "opts", false)
+                    local enabled = false
+                    opts.textobjects = {
+                        select = {
+                            enable = true,
+                            lookahead = true,
+                            keymaps = {
+                                ["af"] = "@function.outer",
+                                ["if"] = "@function.inner",
+                                ["aa"] = "@parameter.outer",
+                                ["ia"] = "@parameter.inner",
+                                ["ac"] = "@class.outer",
+                                ["ic"] = "@class.inner",
+                                ["x"] = "@comment.outer"
+                            },
+                            include_surrounding_whitespace = true,
+                        },
+                        swap = {
+                            enable = true,
+                            swap_next = {
+                                ["<leader>wa"] = "@parameter.inner",
+                            },
+                            swap_previous = {
+                                ["<leader>Wa"] = "@parameter.inner",
+                            },
+                        },
+                        move = {
+                            enable = true,
+                            set_jumps = true,
+                            goto_next_start = {
+                                ["]f"] = "@function.outer",
+                                ["]a"] = "@parameter.inner",
+                            },
+                            goto_next_end = {
+                                ["]F"] = "@function.outer",
+                                ["]A"] = "@parameter.inner",
+                            },
+                            goto_previous_start = {
+                                ["[f"] = "@function.outer",
+                                ["[a"] = "@parameter.inner",
+                            },
+                            goto_previous_end = {
+                                ["[F"] = "@function.outer",
+                                ["[A"] = "@parameter.inner",
+                            },
+                        },
+                    }
+                    require("nvim-treesitter.configs").setup(opts)
+                    if opts.textobjects then
+                        for _, mod in ipairs({ "move", "select", "swap", "lsp_interop" }) do
+                            if opts.textobjects[mod] and opts.textobjects[mod].enable then
+                                enabled = true
+                                break
+                            end
+                        end
+                    end
+                    if not enabled then
+                        require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
+                    end
+                end,
             },
-            indent = { enable = true, disable = { "yaml" } },
-        }
-    },
-    { "p00f/nvim-ts-rainbow" },
-    { "nvim-treesitter/playground" },
-    { "nvim-treesitter/nvim-treesitter-textobjects",
-        config = function() require("nvim-treesitter.configs").setup({
-            textobjects = {
-                select = {
-                    enable = true,
-                    lookahead = true,
-                    keymaps = {
-                        ["af"] = "@function.outer",
-                        ["if"] = "@function.inner",
-                        ["aa"] = "@parameter.outer",
-                        ["ia"] = "@parameter.inner",
-                        ["ac"] = "@class.outer",
-                        ["ic"] = "@class.inner",
-                        ["x"] = "@comment.outer"
-                    },
-                    include_surrounding_whitespace = true,
+        },
+        keys = {
+            { "<c-space>", desc = "Increment selection" },
+            { "<bs>", desc = "Decrement selection", mode = "x" },
+        },
+        ---@type TSConfig
+        opts = {
+            highlight = { enable = true },
+            indent = { enable = true },
+            context_commentstring = { enable = true, enable_autocmd = false },
+            ensure_installed = {
+                "bash",
+                "c",
+                "html",
+                "javascript",
+                "json",
+                "lua",
+                "luadoc",
+                "luap",
+                "markdown",
+                "markdown_inline",
+                "python",
+                "query",
+                "regex",
+                "tsx",
+                "typescript",
+                "vim",
+                "vimdoc",
+                "yaml",
+            },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<C-space>",
+                    node_incremental = "<C-space>",
+                    scope_incremental = false,
+                    node_decremental = "<bs>",
                 },
-                swap = {
-                    enable = true,
-                    swap_next = {
-                        ["<leader>wa"] = "@parameter.inner",
-                    },
-                    swap_previous = {
-                        ["<leader>Wa"] = "@parameter.inner",
-                    },
-                },
-                move = {
-                    enable = true,
-                    set_jumps = true,
-                    goto_next_start = {
-                        ["]f"] = "@function.outer",
-                        ["]a"] = "@parameter.inner",
-                    },
-                    goto_next_end = {
-                        ["]F"] = "@function.outer",
-                        ["]A"] = "@parameter.inner",
-                    },
-                    goto_previous_start = {
-                        ["[f"] = "@function.outer",
-                        ["[a"] = "@parameter.inner",
-                    },
-                    goto_previous_end = {
-                        ["[F"] = "@function.outer",
-                        ["[A"] = "@parameter.inner",
-                    },
-                },
-            }
-        })
-        end
+            },
+        },
+        ---@param opts TSConfig
+        config = function(_, opts)
+            if type(opts.ensure_installed) == "table" then
+                ---@type table<string, boolean>
+                local added = {}
+                opts.ensure_installed = vim.tbl_filter(function(lang)
+                    if added[lang] then
+                        return false
+                    end
+                    added[lang] = true
+                    return true
+                end, opts.ensure_installed)
+            end
+            require("nvim-treesitter.configs").setup(opts)
+        end,
     }
 
 })
