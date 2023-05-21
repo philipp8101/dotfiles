691a6313c8299ae01bbc3b34932fbc19788d44f1
Philipp Conrad <philipp8101@gmail.com>
2023-05-21 20:21:52 +0200

    tmux-sessionizer: added additional filter for window class

--- .scripts/i3/navigate-to-window.py
+++ .scripts/i3/navigate-to-window.py
@@ -3,14 +3,15 @@ import i3ipc
 import sys
 
 title = sys.argv[1]
-if not title: 
+wm_class = sys.argv[2]
+if not title and wm_class: 
     sys.exit(1)
 
 i3 = i3ipc.Connection()
 
 def find_window_by_title(target_title):
     for window in i3.get_tree().leaves():
-        if window.window_title and target_title in window.window_title :
+        if window.window_title and target_title in window.window_title and window.window_class and window.window_class == wm_class:
             return window.id
     sys.exit(2)
 
