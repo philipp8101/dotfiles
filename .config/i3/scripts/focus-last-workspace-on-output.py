0a35e0bf1fc03ae2d8c3654185dc78cdef6e10df
Philipp Conrad <philipp8101@gmail.com>
2023-05-31 23:02:54 +0200

    i3: fix focus-script to use workspace instead of focus command

--- .config/i3/scripts/focus-last-workspace-on-output.py
+++ .config/i3/scripts/focus-last-workspace-on-output.py
@@ -4,14 +4,15 @@ import i3ipc
 i3 = i3ipc.Connection()
 
 
-def find_focued_workspace():
+def find_focued_monitor():
     focused = i3.get_tree().find_focused()
     for i in i3.get_tree().nodes:
         result = i.find_by_id(focused.id) 
         if result:
-            return i.name
+            return i
 
-target = find_focued_workspace()
+target = find_focued_monitor()
 if target:
-    print(target)
-    i3.command("[con_mark={}] focus".format(target))
+    ws_name = i3.get_tree().find_marked(target.name)[0].name
+    if ws_name:
+        i3.command("workspace {}".format(ws_name))
