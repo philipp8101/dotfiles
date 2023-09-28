ea0d2436bf3aba0485da1f1d6a1ce2f77277c2ca
Philipp Conrad <philipp8101@gmail.com>
2023-09-28 21:42:08 +0200

    changed openrgb device ids

--- .scripts/openrgb.sh
+++ .scripts/openrgb.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 case $1 in
 	"--on" )
-		openrgb -d 0 -m "Rainbow Wave" -d 1 -m "Rainbow Wave" -d 3 -m "Rainbow" > /dev/null
+		openrgb -d 1 -m "Rainbow Wave" -d 2 -m "Rainbow Wave" -d 4 -m "Rainbow" > /dev/null
 		;;
 	"--off" )
-		openrgb -d 0 -m "Direct" -d 1 -m "Direct" -d 3 -m "Direct" > /dev/null
+		openrgb -d 1 -m "Direct" -d 2 -m "Direct" -d 4 -m "Direct" > /dev/null
 		;;
 
 esac
