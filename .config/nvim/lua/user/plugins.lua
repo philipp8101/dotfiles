2e25a2317082fe4fc0add81e2c49a674989764a5
Philipp Conrad <philipp8101@gmail.com>
2023-09-28 21:40:45 +0200

    nvim: hoogle

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -579,6 +579,13 @@ require("lazy").setup({
         end
     },
 
+    {
+        "psiska/telescope-hoogle.nvim",
+        config = function()
+            require("telescope").load_extension("hoogle")
+        end,
+    },
+
     { "mfussenegger/nvim-jdtls" },
 
     -- Telescope
@@ -597,6 +604,19 @@ require("lazy").setup({
                     "--column",
                     "--smart-case",
                 },
+            },
+            extensions = {
+                hoogle = {
+                    render = 'default',       -- Select the preview render engine: default|treesitter
+                    -- default = simple approach to render the document
+                    -- treesitter = render the document by utilizing treesitter's html parser
+                    renders = {               -- Render specific options
+                        treesitter = {
+                            remove_wrap = false   -- Remove hoogle's own text wrapping. E.g. if you uses neovim's buffer wrapping
+                            -- (autocmd User TelescopePreviewerLoaded setlocal wrap)
+                        }
+                    }
+                }
             }
         }
     },
