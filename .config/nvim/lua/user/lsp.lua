07ab9290b3b43d4addc947dbdbff98fe04e725d7
Philipp Conrad <philipp8101@gmail.com>
2024-01-02 20:21:11 +0100

    nvim: lua_ls set vim global

--- .config/nvim/lua/user/lsp.lua
+++ .config/nvim/lua/user/lsp.lua
@@ -2,3 +2,13 @@ require('lspconfig')['hls'].setup{
   filetypes = { 'haskell', 'lhaskell', 'cabal' },
   cmd = { "haskell-language-server-wrapper", "--lsp" }
 }
+
+require('lspconfig')['lua_ls'].setup{
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = {"vim"},
+      },
+    },
+  },
+}
