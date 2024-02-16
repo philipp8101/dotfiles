d882caffd64f4d4eb9dcca5b516cb6c178c14a41
Philipp Conrad <philipp8101@gmail.com>
2024-02-16 18:27:58 +0100

    nvim: added neorg

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -67,6 +67,27 @@ require("lazy").setup({
     },
 
     {
+        "nvim-neorg/neorg",
+        build = ":Neorg sync-parsers",
+        lazy = false, -- specify lazy = false because some lazy.nvim distributions set lazy = true by default
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {
+            load = {
+                ["core.defaults"] = {}, -- Loads default behaviour
+                ["core.concealer"] = {}, -- Adds pretty icons to your documents
+                ["core.dirman"] = { -- Manages Neorg workspaces
+                    config = {
+                        workspaces = {
+                            notes = "~/notes",
+                        },
+                        default_workspace = "notes",
+                    },
+                },
+            },
+        }
+    },
+
+    {
         "windwp/nvim-ts-autotag",
         dependencies = { "nvim-treesitter/nvim-treesitter" },
         lazy = true,
