521764cc9164b7d06eff35011f1c296c48d624a3
Philipp Conrad <philipp8101@gmail.com>
2024-06-29 11:03:30 +0200

    enable i3 configuration

--- configuration.nix
+++ configuration.nix
@@ -149,32 +149,25 @@ in
 		displayManager.defaultSession = "hyprland";
 		xserver.videoDrivers = [ "nvidia" ];
 
-		# xserver = {
-		# 	enable = true;
-		# 	xkb.layout = "de";
-		# 	resolutions = [
-		# 	{ x = 1920; y = 1080; }
-		# 	];
-		# 	desktopManager = {
-		# 		xterm.enable = false;
-		# 		plasma6.enable = true;
-		# 	};
-		# 	displayManager = {
-		# 		defaultSession = "none+i3";
-		# 	};
-		# 	windowManager.i3 = {
-		# 		enable = true;
-		# 		extraPackages = with pkgs; [
-		# 			dmenu
-		# 				i3status
-		# 				i3lock
-		# 				i3blocks
-		# 				polybarFull
-		# 				dunst
-		# 				rofi
-		# 		];
-		# 	};
-		# };
+		xserver = {
+			enable = true;
+			xkb.layout = "de";
+			resolutions = [
+			{ x = 1920; y = 1080; }
+			];
+			windowManager.i3 = {
+				enable = true;
+				extraPackages = with pkgs; [
+					dmenu
+						i3status
+						i3lock
+						i3blocks
+						polybarFull
+						dunst
+						rofi
+				];
+			};
+		};
 	};
 	fonts.packages = with pkgs; [
 		inconsolata
