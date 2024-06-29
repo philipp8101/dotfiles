fff23c366145ffc6a1ea20f2526a447a74bda792
Philipp Conrad <philipp8101@gmail.com>
2024-06-29 11:02:45 +0200

    add auth agent for hyprland

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -26,6 +26,9 @@ in
         };
         sensitivity = 0;
       };
+      exec-once = [
+        "${pkgs.kdePackages.polkit-kde-agent-1}/libexec/polkit-kde-authentication-agent-1"
+      ];
       monitor = [
         "DP-1, 1920x1080, 0x0, 1"
         "DP-2, 1920x1080@144, 1920x0, 1"
