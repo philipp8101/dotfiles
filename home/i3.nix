e38325180b176e64c7c4c4b08ee2603607666e37
Philipp Conrad <philipp8101@gmail.com>
2024-07-01 12:34:37 +0200

    move i3scripts to separate module

--- home/i3.nix
+++ home/i3.nix
@@ -13,17 +13,7 @@ rofi-power-menu = pkgs.stdenv.mkDerivation {
     chmod +x $out/bin/rofi-power-menu
     '';
 };
-scripts = pkgs.stdenv.mkDerivation {
-  name = "i3-scripts";
-  src = ./i3scripts;
-  buildInputs = with pkgs; [ gnused python311 python311Packages.i3ipc ];
-  buildCommand = ''
-    mkdir -p $out/bin
-    cp $src/*py $out/bin/
-    chmod +x $out/bin/*
-    sed -i -e '1i #!${pkgs.python312}/bin/python' $out/bin/*
-  '';
-};
+scripts = import ./i3scripts.nix {inherit pkgs;};
 in
 {
   xsession.windowManager.i3 = {
---
+++ home/i3scripts.nix
@@ -0,0 +1,12 @@
+{pkgs, ...}:
+pkgs.stdenv.mkDerivation {
+  name = "i3-scripts";
+  src = ./i3scripts;
+  buildInputs = with pkgs; [ gnused python311 python311Packages.i3ipc ];
+  buildCommand = ''
+    mkdir -p $out/bin
+    cp $src/*py $out/bin/
+    chmod +x $out/bin/*
+    sed -i -e '1i #!${pkgs.python312}/bin/python' $out/bin/*
+  '';
+}
