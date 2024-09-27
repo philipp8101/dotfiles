3763d352ac822e16a25418b5cb994311704aac63
Philipp Conrad <philipp8101@gmail.com>
2024-09-27 12:43:27 +0200

    update nixvim plugin options

--- nixvim/config/conform.nix
+++ nixvim/config/conform.nix
@@ -1,26 +1,28 @@
 {
     plugins.conform-nvim = {
         enable = true;
-        formatOnSave = {
-            lspFallback = true;
-            timeoutMs = 500;
-        };
-        formattersByFt = {
-            javascript = [ "prettier" ];
-            typescript = [ "prettier" ];
-            css = [ "prettier" ];
-            html = [ "prettier" ];
-            json = [ "prettier" ];
-            yaml = [ "prettier" ];
-            markdown = [ "prettier" ];
-            graphql = [ "prettier" ];
-            lua = [ "stylua" ];
-            python = [ "black" ];
-            bash = [ "beautysh" ];
-            sh = [ "beautysh" ];
-            zsh = [ "beautysh" ];
-            latex = [ "latexindent" ];
-            cmake = [ "cmakelang" ];
+        settings = {
+            format_on_save = {
+                lspFallback = true;
+                timeoutMs = 500;
+            };
+            formattersByFt = {
+                javascript = [ "prettier" ];
+                typescript = [ "prettier" ];
+                css = [ "prettier" ];
+                html = [ "prettier" ];
+                json = [ "prettier" ];
+                yaml = [ "prettier" ];
+                markdown = [ "prettier" ];
+                graphql = [ "prettier" ];
+                lua = [ "stylua" ];
+                python = [ "black" ];
+                bash = [ "beautysh" ];
+                sh = [ "beautysh" ];
+                zsh = [ "beautysh" ];
+                latex = [ "latexindent" ];
+                cmake = [ "cmakelang" ];
+            };
         };
     };
 }
--- nixvim/config/treesitter.nix
+++ nixvim/config/treesitter.nix
@@ -1,6 +1,6 @@
 {
     plugins.treesitter = {
         enable = true;
-        ensureInstalled = "all";
+        settings.ensureInstalled = "all";
     };
 }
