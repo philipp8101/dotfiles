67bb6ae0152b26cd277ceaccc34b6651f7d88500
Philipp Conrad <philipp8101@gmail.com>
2024-11-07 00:23:43 +0100

    adjust resolution

--- host/surface/home.nix
+++ host/surface/home.nix
@@ -5,6 +5,9 @@ in
     {
       identifier = "eDP-1";
       isPrimary = true;
+      resolution = "2736x1824";
+      refreshrate = "59.96";
+      scale = "2";
     }
   ];
   wayland.windowManager.hyprland = {
