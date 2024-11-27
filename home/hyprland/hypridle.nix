1afd6608ffb57660a92fef456a9fd8a991655694
Philipp Conrad <philipp8101@gmail.com>
2024-11-27 16:44:17 +0100

    hyprlock

--- home/hyprland/hypridle.nix
+++ home/hyprland/hypridle.nix
@@ -10,7 +10,7 @@ in
         after_sleep_cmd = "hyprctl dispatch dpms on";
         before_sleep_cmd = "loginctl lock-session";
         ignore_dbus_inhibit = false;
-        lock_cmd = "${save-screenshots}/bin/save-screenshots ; ${pkgs.hyprlock}/bin/hyprlock";
+        lock_cmd = "${save-screenshots}/bin/save-screenshots ; ${pkgs.hyprlock}/bin/hyprlock --immediate";
       };
 
       listener = [
