59f221a9ca2ba3a2f85a3441e23f96ad8b805782
Philipp Conrad <philipp8101@gmail.com>
2025-04-02 13:44:43 +0200

    sway-audio-idle-inhibit as systemd user service

--- configuration/hyprland.nix
+++ configuration/hyprland.nix
@@ -10,4 +10,14 @@
     ];
   };
   services.displayManager.defaultSession = pkgs.lib.mkIf config.programs.hyprland.enable "hyprland";
+  systemd.user.services.sway-audio-idle-inhibit = {
+    enable = true;
+    wantedBy = [ "default.target" ];
+    description = "prevent suspending system when audio is playing";
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = ''${pkgs.sway-audio-idle-inhibit}/bin/sway-audio-idle-inhibit'';
+      Restart = "on-failure";
+    };
+  };
 }
