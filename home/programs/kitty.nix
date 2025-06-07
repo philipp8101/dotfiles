ee31ed0400e5d30dfad52b89048f005417c0f8d5
Philipp Conrad <philipp8101@gmail.com>
2025-06-07 16:58:33 +0200

    kitty disable bell

--- home/programs/kitty.nix
+++ home/programs/kitty.nix
@@ -8,6 +8,7 @@
     };
     settings = {
       background_opacity = "0.8";
+      enable_audio_bell = false;
     };
     extraConfig = ''
     map ctrl+shift+p>f kitten hints --type path --program @
