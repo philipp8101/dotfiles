5ec2eec3d3ce95c5a2ce5cf83367c336fcde2804
Philipp Conrad <philipp8101@gmail.com>
2025-06-07 16:59:59 +0200

    remove specialisation

--- configuration/gaming.nix
+++ configuration/gaming.nix
@@ -1,18 +1,13 @@
 { pkgs, ... }:
 {
-  specialisation = {
-    gaming.configuration = {
-      programs.steam = {
-        enable = true;
-        remotePlay.openFirewall = true;
-        dedicatedServer.openFirewall = true;
-      };
-      environment.systemPackages = with pkgs; [
-        lutris
-        bottles
-        prismlauncher
-      ];
-    };
-    default.configuration = {};
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
   };
+  environment.systemPackages = with pkgs; [
+    lutris
+    bottles
+    prismlauncher
+  ];
 }
