885ad03ca8fabe5c921d49ea208950fa90eedb54
Philipp Conrad <philipp8101@gmail.com>
2025-06-24 21:03:47 +0200

    new udev rules

--- configuration/default.nix
+++ configuration/default.nix
@@ -90,8 +90,12 @@
     ddccontrol.enable = true;
     udev = {
       enable = true;
-      # steelseries for headsetctl
-      extraRules = ''KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1038", ATTRS{idProduct}=="12c2", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"'';
+      extraRules = builtins.concatStringsSep "\n" [
+        # steelseries for headsetctl
+        ''KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1038", ATTRS{idProduct}=="12c2", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"''
+        # logic analyser
+        ''SUBSYSTEM=="usb", ATTR{idVendor}=="0925", ATTR{idProduct}=="3881", MODE="0666", GROUP="plugdev"''
+      ];
     };
   };
   fonts.packages = with pkgs; [
