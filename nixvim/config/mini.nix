68b3dea6e7c1413fda6934276eed884dbd7526fc
Philipp Conrad <philipp8101@gmail.com>
2025-06-24 21:03:59 +0200

    mini.files open current files directory

--- nixvim/config/mini.nix
+++ nixvim/config/mini.nix
@@ -9,7 +9,7 @@
   keymaps = lib.mkIf config.plugins.mini.enable [
     {
       key = "<leader>X";
-      action = helpers.mkRaw "require'mini.files'.open";
+      action = helpers.mkRaw "function() require'mini.files'.open(vim.api.nvim_buf_get_name(0)) end";
       options.desc = "Open Mini.Files";
     }
   ];
