4a32b91c20f20ca346287ae78acfe38262278846
Philipp Conrad <philipp8101@gmail.com>
2025-09-04 00:00:15 +0200

    dispaly

--- host/desktop/home.nix
+++ host/desktop/home.nix
@@ -1,18 +1,18 @@
 {
   displays = [
-    # {
-    #   identifier = "DP-1";
-    # }
+    {
+      identifier = "DP-1";
+    }
     {
       identifier = "DP-2";
       isPrimary = true;
       offset = "1920x0";
       refreshrate = 144.0;
     }
-    {
-      identifier = "HDMI-A-1";
-      # offset = "3840x0";
-    }
+    # {
+    #   identifier = "HDMI-A-1";
+    #   offset = "3840x0";
+    # }
   ];
   wayland.windowManager.hyprland.enable = true;
 }
