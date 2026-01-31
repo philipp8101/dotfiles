d0cacc19319df01ad1cdb74b1f385c65aa831c4c
Philipp Conrad <philipp8101@gmail.com>
2026-01-31 19:49:54 +0100

    backport to linux 6.15.9 commit 8851e27d2cb947ea8bbbe8e812068f7bf5cbd00b Author: Miguel Ojeda <ojeda@kernel.org> Date:   Fri Aug 29 21:55:25 2025 +0200

--- host/surface/default.nix
+++ host/surface/default.nix
@@ -8,4 +8,8 @@
     ./touchscreen.nix
   ];
   hardware.microsoft-surface.kernelVersion = "stable";
+  boot.kernelPatches = [{
+    name = "./linux-6.15.9-generate_rust_target_rs.patch";
+    patch = ./linux-6.15.9-generate_rust_target_rs.patch;
+  }];
 }
