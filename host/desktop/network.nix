7617183a1dcd882a270118601d4d6f520a7b0135
Philipp Conrad <philipp8101@gmail.com>
2026-03-11 15:01:56 +0100

    networkmanager vpn hook

--- host/desktop/network.nix
+++ host/desktop/network.nix
@@ -1,6 +1,22 @@
+{ pkgs, ... }:
 {
   networking = {
     hostId = "e52e59bb";
     hostName = "desktop";
+    # networkmanager.dispatcherScripts = 
+    #   let source = pkgs.writeText "hook" ''
+    #   case "$2" in
+    #     vpn-up)
+    #       echo "$1:$2: running vpn up hook"
+    #       ;;
+    #     vpn-down|vpn-pre-down)
+    #       echo "$1:$2: running vpn (pre) down hook"
+    #       ;;
+    #     esac
+    #   '';
+    #   in [ 
+    #   { type = "pre-down"; inherit source; }
+    #   { inherit source; }
+    #   ];
   };
 }
