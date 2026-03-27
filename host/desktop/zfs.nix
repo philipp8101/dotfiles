0f56c688ed898a27f86a7d1149e43a9ab36cf545
Philipp Conrad <philipp8101@gmail.com>
2026-03-27 01:47:22 +0100

    add zpool

--- host/desktop/zfs.nix
+++ host/desktop/zfs.nix
@@ -3,7 +3,7 @@
     supportedFilesystems = [ "zfs" ];
     zfs = {
       forceImportRoot = false;
-      extraPools = [ "tank" ];
+      extraPools = [ "tank" "game" ];
       # list of encrypted datasets that should be mounted on boot
       # defaults to true which means all encrypted datasets will be mounted
       # https://github.com/NixOS/nixpkgs/blob/f771eb401a46846c1aebd20552521b233dd7e18b/nixos/modules/tasks/filesystems/zfs.nix#L391
