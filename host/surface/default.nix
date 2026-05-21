2b241bfa959c1fcbf1cf418dcffa5c7ec1034ae6
Philipp Conrad <philipp8101@gmail.com>
2026-05-21 18:32:16 +0200

    surface wg key

--- host/surface/default.nix
+++ host/surface/default.nix
@@ -1,11 +1,8 @@
-{ pkgs, ... }:
-{
-  imports = [
-    ./hardware-configuration.nix
-    ./luks.nix
-    ./network.nix
-    ./kmonad.nix
-    ./touchscreen.nix
-  ];
-  hardware.microsoft-surface.kernelVersion = "stable";
-}
+{ lib, ... }:
+let
+  files = lib.attrNames (builtins.readDir ./.);
+  nixFiles = lib.filter (f: lib.hasSuffix ".nix" f 
+                        && !lib.hasSuffix "default.nix" f 
+                        && !lib.hasSuffix "home.nix" f) files;
+  imports = lib.map (f: ./${f}) nixFiles;
+in { imports = imports; }
---
+++ host/surface/surface-kernel.nix
@@ -0,0 +1,3 @@
+{
+  hardware.microsoft-surface.kernelVersion = "stable";
+}
