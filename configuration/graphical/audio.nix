da58640077f159b55f8a241c844335cdf76f1b95
Philipp Conrad <philipp8101@gmail.com>
2026-08-20 10:08:05 +0200

    high sample rate

--- configuration/graphical/audio.nix
+++ configuration/graphical/audio.nix
@@ -6,5 +6,9 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
+    extraConfig.pipewire."10-clock-rate"."context.properties" = {
+      "default.clock.rate" = 192000;
+      "default.clock.allowed-rate" = [ 41000 48000 88200 96000 192000 ];
+    };
   };
 }
