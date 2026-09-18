a19688e196ab0874bf079284a0b8a4790c5a4ad3
Philipp Conrad <philipp8101@gmail.com>
2026-09-18 21:05:20 +0200

    zfs dataset mounts

--- host/desktop/nfs-client.nix
+++ host/desktop/nfs-client.nix
@@ -4,4 +4,16 @@
     device = "192.168.178.178:/tank";
     fsType = "nfs4";
   };
+  fileSystems."/tank/savegames" = {
+    device = "192.168.178.178:/tank/savegames";
+    fsType = "nfs4";
+  };
+  fileSystems."/tank/documents" = {
+    device = "192.168.178.178:/tank/documents";
+    fsType = "nfs4";
+  };
+  fileSystems."/tank/con" = {
+    device = "192.168.178.178:/tank/con";
+    fsType = "nfs4";
+  };
 }
--- host/nas/nfs-server.nix
+++ host/nas/nfs-server.nix
@@ -1,7 +1,12 @@
 {
   services.nfs.server.enable = true;
+  # https://wiki.nixos.org/wiki/ZFS#NFS_share
+  # using `zfs set share=on <pool>`
   services.nfs.server.exports = ''
-    /tank    192.168.178.0/24(insecure,rw,sync,no_subtree_check)
+    /tank              192.168.178.0/24(insecure,rw,sync,no_subtree_check)
+    /tank/con          192.168.178.0/24(insecure,rw,sync,no_subtree_check)
+    /tank/savegames    192.168.178.0/24(insecure,rw,sync,no_subtree_check)
+    /tank/documents    192.168.178.0/24(insecure,rw,sync,no_subtree_check)
   '';
   # nfsv3 requires different ports !
   # https://wiki.nixos.org/wiki/NFS#Firewall
